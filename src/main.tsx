df9cad22af2797f582296d53876888f851ec4829
abdullahghoneim <abdullahghoneim890@gmail.com>
2023-07-11 14:58:51 +0300

    route config

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root.tsx";
+import ErrorPage from "./error-page.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
-  },
-  {
-    path: "/about",
-    element: <div>About!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
